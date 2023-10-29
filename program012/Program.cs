655ac1de3b327645ee4e8a4f7a3b43470bda5a16
Zpitoiti <2008dima15@gmail.com>
2023-10-29 12:44:32 +0700

    программа 012

---
+++ program012/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.Write("Введите число: "); 
+int num1 = int.Parse(Console.ReadLine());
+
+int Number(int num)
+{
+    int sum = 0;
+    for (int i = 0; i < num; i++)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int resalt = Number(num1);
+System.Console.WriteLine($"Сумма цифр в этом числе равна {resalt}.");

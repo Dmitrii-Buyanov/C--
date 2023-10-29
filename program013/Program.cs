fa67dd48922bfd5766ac25940b46004262f910fb
Zpitoiti <2008dima15@gmail.com>
2023-10-29 13:15:03 +0700

    программа 013

--- program013/Program.cs
+++ program013/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] InArr(int a)
+{
+    int[] array = new int[a];
+    for (int i = 0; i < a; i++)
+    {
+        array[i] = new Random().Next(-10, 100);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+}
+
+
+int[] result = InArr(8);
+PrintArray(result);

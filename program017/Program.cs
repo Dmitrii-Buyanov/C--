31d162391b4e88da2cd1925a04c469f2610ee882
Zpitoiti <2008dima15@gmail.com>
2023-11-04 20:33:22 +0700

    018

--- program017/Program.cs
+++ program017/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] GetArray(int len, int a, int b)
+{
+int[] array = new int[len];
+b++; // Генерировать будем до b включительно
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = rand.Next(a, b);
+}
+return array;
+}
+
+void PrintArray(int[] array)
+{
+foreach (int num in array)
+{
+System.Console.Write("{0, 3}", num);
+}
+System.Console.WriteLine();
+
+}
+
+void ReplaceElements(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = -array[i];
+}
+}
+
+int[] arr = GetArray(20, -9, 9);
+PrintArray(arr);
+ReplaceElements(arr);
+PrintArray(arr);

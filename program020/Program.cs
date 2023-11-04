fc40509d47b142a156867e052268fce32dd3e425
Zpitoiti <2008dima15@gmail.com>
2023-11-04 21:20:52 +0700

    021

---
+++ program020/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string Input(string message)
+{
+  System.Console.Write(message);
+  string result = Console.ReadLine()!; 
+  return result;
+}
+
+bool IsPallindrome(string str)
+{
+  str = str.ToLower(); 
+  for (int i = 0; i < str.Length; i++)
+  {
+    if (str[i] != str[str.Length - 1 - i]) return false;
+  }
+  return true;
+}
+
+string str = Input("Введите строку: ");
+System.Console.WriteLine(IsPallindrome(str) ? "Да, является" : "Нет, не является");

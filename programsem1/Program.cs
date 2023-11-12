58eec2d50ad899c285f445c25071949f7d15f192
Zpitoiti <2008dima15@gmail.com>
2023-11-12 13:14:31 +0700

    програма семенара 2

--- programsem1/Program.cs
+++ programsem1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// programsem1
+int[,] GetArray(int m, int n, int min, int max)
+{
+int[,] array = new int[m, n];
+Random rand = new Random();
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+array[i, j] = rand.Next(min, max);
+}
+}
+return array;
+}
+
+void PrintArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write(array[i, j] + " ");
+}
+Console.WriteLine();
+}
+}
+
+int[,] arr = GetArray(3, 4, 0, 10);
+PrintArray(arr);

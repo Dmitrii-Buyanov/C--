93ba8e9df5a42205951092516587f9148a702e42
Zpitoiti <2008dima15@gmail.com>
2023-11-12 12:59:48 +0700

    programsem1

--- program021/Program.cs
+++ program021/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+using System.Xml.XPath;
+
+string Input(string message)
+{
+  System.Console.Write(message);
+  string result = Console.ReadLine()!; L
+  return result;
+}
+
+
+string ToLower(string str)
+{
+  string result = string.Empty;
+  for (int i = 0; i < str.Length; i++)
+  {
+    if(str[i] >= 'A' && str[i] <= 'Z') result += (char)(str[i] + 32);
+    else result +=str[i];
+  }
+  return result;
+}
+
+string str = Input("Введите строку: ");
+System.Console.WriteLine(ToLower(str));

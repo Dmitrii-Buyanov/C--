12986f18406fe4ff80ceb91444a77b35e494aef3
Zpitoiti <2008dima15@gmail.com>
2023-11-18 21:25:17 +0700

    2

---
+++ Hwgirliosdfhaahj;sdf/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Вычисление длины пути торможения автомобиля");
+Console.Write("Введите начальную скорость авто (м/c) = ");
+int v0 = Convert.ToInt32(Console.ReadLine());
+formul = 0;
+formul = v0 * v0 / 10;
+Console.WriteLine("До полной остановки автомобиль пройдёт = {formul} ");

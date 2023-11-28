89d5b35e0a92de6ffb8cc37c99d0ee3645cf653b
Zpitoiti <2008dima15@gmail.com>
2023-11-28 21:25:46 +0700

    атестация крико каго естрипер и как же без кавасаки

--- program005/Program.cs
+++ program005/Program.cs
@@ -1,11 +1,1 @@
-﻿Console.WriteLine("Вычисление длины пути торможения автомобиля ");
-Console.Write("Введите начальную скорость авто (м/c) = ");
-int v0 = Convert.ToInt32(Console.ReadLine());
-if (string != " ")
-{
-    Console.WriteLine("Введите число");
-}
-int formul = 0;
-formul = v0 * v0 / 10;
-Console.Write("До полной остановки автомобиль пройдёт = ");
-Console.Write(formul);
+﻿
